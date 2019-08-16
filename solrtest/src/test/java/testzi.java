47cef8b6fe54a831f06e15fceccc1d2d62b5b11b
sunlong <603941574@qq.com>
2019-08-16 17:10:11 +0800

    进缓存区

---
+++ solrtest/src/test/java/testzi.java
@@ -0,0 +1,16 @@
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class testzi extends testfu {
+
+    @Override
+    public void mathod(){
+        System.out.println("zi");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

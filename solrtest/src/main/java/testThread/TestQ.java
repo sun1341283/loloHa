20ec3a8c3cc9296cdf16c70656bb7a64323fdf10
sunlong <603941574@qq.com>
2019-08-16 16:50:30 +0800

    测试dev

---
+++ solrtest/src/main/java/testThread/TestQ.java
@@ -0,0 +1,46 @@
+package testThread;
+
+import org.apache.commons.collections.list.SynchronizedList;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class TestQ {
+    public static void test(String a){
+        System.out.println(a);
+    }
+
+
+    public static void main(String[] args) {
+        int i=0;
+        for (test("a"); i < 2 ; test("b")) {
+            test("d");
+            i++;
+            System.out.println(i);
+        }
+
+    }
+
+    @Test
+    public void testSynchonized(){
+        ArrayList<String> objects = new ArrayList<String>();
+        List<String> strings = Collections.synchronizedList(objects);
+        strings.add("1");
+        strings.add("0");
+        strings.add("2");
+        Iterator<String> iterator = strings.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        Collections.sort(strings);
+
+
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+    }
+}
